b690e840c7ee9a7fe9e028958c9d0c3f2433cc4d
GoldenPing <74360915+GoldenPing@users.noreply.github.com>
2022-09-29 13:30:17 +0200

    set() : mise en ligne

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$pizzas = [
+    "1" => [
+        "name" => "Reine",
+        "base" => "Tomate",
+        "prix" => 10.90,
+        "ingredient" => [
+            "champi",
+            "mozza",
+            "jambon",
+            "olive",
+        ]
+    ],
+    "2" => [
+
+    ]
+]
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Pizza Gang</title>
+</head>
+<body>
+
+<ul>
+    <li>
+
+    </li>
+</ul>
+
+
+</body>
+</html>
+
+
+
